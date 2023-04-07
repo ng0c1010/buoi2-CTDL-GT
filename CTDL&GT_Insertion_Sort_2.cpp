7ed981968eb8d13e04e6545a35ab80e9c0533a04
ng0c1010 <96835034+ng0c1010@users.noreply.github.com>
2023-04-07 08:03:13 +0700

    Update CTDL&GT_Insertion_Sort_2.cpp

--- CTDL&GT_Insertion_Sort_2.cpp
+++ CTDL&GT_Insertion_Sort_2.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
-    for(int i = 0; i < n-1; i++) {
-        int minIndex = i;
-        for(int j = i+1; j < n; j++) {
-            if(arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
+void insertionSort(int arr[], int n) {
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
         }
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+        arr[j+1] = key;
     }
 }
 
@@ -40,11 +38,12 @@ int main() {
     cout << "Mang truoc khi sap xep: ";
     xuatMang(arr, n);
 
-    selectionSort(arr, n);
+    insertionSort(arr, n);
 
     cout << "Mang sau khi sap xep: ";
     xuatMang(arr, n);
 
     return 0;
 }
+
 

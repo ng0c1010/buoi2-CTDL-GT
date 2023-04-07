2b7a37a92064ccacaabe10cbd81ecc0ecbb09e5e
ng0c1010 <96835034+ng0c1010@users.noreply.github.com>
2023-04-07 07:57:17 +0700

    Update CTDL&GT_linear_search_2.cpp

--- CTDL&GT_linear_search_2.cpp
+++ CTDL&GT_linear_search_2.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int linearSearch(int arr[], int n, int target) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == target) {
+int linearSearch(int arr[], int n, int x) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == x) {
             return i;
         }
     }
     return -1;
 }
 
-int main() {
-    int arr[] = {3, 6, 1, 8, 2};
-    int target = 8;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int result = linearSearch(arr, n, target);
-    if (result == -1) {
-        cout << "khong tim thay phan tu" << endl;
-    } else {
-        cout << "tim thay phan tu tai vi tri " << result << endl;
+void nhapMang(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << "Nhap gia tri arr[" << i << "]: ";
+        cin >> arr[i];
     }
+}
+
+int main() {
+    int n;
+    cout << "Nhap so phan tu cua mang: ";
+    cin >> n;
+    int arr[n];
+
+    nhapMang(arr, n);
+
+    int x;
+    cout << "Nhap gia tri can tim: ";
+    cin >> x;
+
+    int result = linearSearch(arr, n, x);
+    if(result == -1) {
+        cout << "Khong tim thay " << x << " trong mang.\n";
+    }
+    else {
+        cout << "Tim thay " << x << " tai vi tri " << result << ".\n";
+    }
+
     return 0;
 }
 

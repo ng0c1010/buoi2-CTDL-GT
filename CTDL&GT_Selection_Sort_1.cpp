77a66590a34dfac30d2e2f00a239474684d85575
ng0c1010 <96835034+ng0c1010@users.noreply.github.com>
2023-04-07 07:39:00 +0700

    Add files via upload

---
+++ CTDL&GT_Selection_Sort_1.cpp
@@ -0,0 +1,14 @@
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        int minIndex = i;
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+cai dat thuat toan selection sort , link tham khao :Chat gpt

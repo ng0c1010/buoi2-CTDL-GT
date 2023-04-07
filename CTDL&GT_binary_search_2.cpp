a97cc718d6e36687d91ff2a7efca6fa07744e69d
ng0c1010 <96835034+ng0c1010@users.noreply.github.com>
2023-04-07 07:08:26 +0700

    Add files via upload

---
+++ CTDL&GT_binary_search_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10};
+    int target = 8;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, n, target);
+    if (result == -1) {
+        cout << "Element not found" << endl;
+    } else {
+        cout << "Element found at index " << result << endl;
+    }
+    return 0;
+}
